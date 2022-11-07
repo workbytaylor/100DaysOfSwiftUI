2ad0061b5c50477c4c90221c4a1b5eee7342c72e
workbytaylor <79762858+workbytaylor@users.noreply.github.com>
2022-11-06 19:57:10 -0500

    Day 71 completion
    Download JSON from wikipedia.

---
+++ Project 14/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Nilakshi Roy on 2022-08-14.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
