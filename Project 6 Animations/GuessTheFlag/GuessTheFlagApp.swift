555d6787d104768af681b020590374eb07ea7082
workbytaylor <79762858+workbytaylor@users.noreply.github.com>
2023-09-23 12:39:18 -0400

    Commit

---
+++ Project 6 Animations/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Nilakshi Roy on 2022-05-24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
